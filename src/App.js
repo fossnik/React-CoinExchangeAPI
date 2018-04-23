5afc57825b25bf7a54179a7a2055f8636c252d14
Zack Hartmann <zackhartmann@gmail.com>
2018-04-23 13:33:33 -0400

    Revised Page Title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">CoinExchange API with React</h1>
         </header>
 		<GetMarkets />
 	  </div>
